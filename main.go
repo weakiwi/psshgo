008352264ded9b68ebe750ce395e50407c40e43b
git <dengyi0215@gmail.com>
2018-11-22 11:16:24 +0800

    mod

--- main.go
+++ main.go
@@ -189,43 +189,12 @@ func pssh(c *cli.Context) {
     command := mustGetStringVar(c, "c")
 
     //var t *testing.T
-    fi, err := os.Open(hostfile)
-    if err != nil {
-        fmt.Printf("Error: %s\n", err)
-        return
-    }
-    br := bufio.NewReader(fi)
     counter := ComputeLine(hostfile)
     done := make(chan string, counter)
-    for {
-        line, err := br.ReadString('\n')
-	    var myconfig sshconfig
-        if err != nil || err == io.EOF {
-            break
-        }
-        if strings.Contains(string(line), "@") && strings.Contains(string(line), ":") {
-                s := strings.Split(string(line), "@")
-                myconfig.user = s[0]
-                s1 := strings.Split(s[1], ":")
-                myconfig.address = s1[0]
-                myconfig.port = s1[1]
-        } else if strings.Contains(string(line), ":") == false  && strings.Contains(string(line), "@"){
-                s := strings.Split(string(line), "@")
-                myconfig.user = s[0]
-                myconfig.address = s[1]
-                myconfig.port = "22"
-        } else if strings.Contains(string(line), "@") == false && strings.Contains(string(line), ":"){
-                myconfig.user = "root"
-                s := strings.Split(string(line), ":")
-                myconfig.address = s[0]
-                myconfig.port = s[1]
-        } else {
-                myconfig.user = "root"
-                myconfig.address = strings.Replace(string(line), "\n", "", -1)
-                myconfig.port = "22"
-        }
-        waitgroup.Add(1)
-        go sshexec(&myconfig, command, done)
+    myconfigs, err := parseHostfile(hostfile)
+    for i := range myconfigs {
+            waitgroup.Add(1)
+        go sshexec(&myconfigs[i], command, done)
     }
     waitgroup.Wait()
 	for v := range done {
