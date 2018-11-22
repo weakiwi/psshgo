b9ec0e58dd85f93ec36fa384b20eadebcbec788e
weakiwi <670932228@qq.com>
2018-11-22 20:39:17 +0800

    add playbook like ini support
    add playbook like ini support

--- parseini.go
+++ parseini.go
@@ -1,43 +1,125 @@
-package pssh
+package main
 
 import (
-        "os"
-        "log"
-        "github.com/go-ini/ini"
+	"bufio"
+	"fmt"
+	"github.com/go-ini/ini"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
 
-func parseini(path string) (scp_playbooks []playbook_scp, ssh_playbooks []playbook_ssh, err error){
-        cfg, err := ini.Load(path)
-        if err != nil {
-            log.Fatalf("open ini err: %v", err)
-            return nil, nil, err
-            os.Exit(1)
-        }
-        secs := cfg.Sections()
-        for i := range secs {
-                ini_type := secs[i].Key("type").String()
-                if ini_type == "ssh" {
-                    var my_playbook_ssh playbook_ssh
-                    log.Println("command is : %v", secs[i].Key("command"))
-                    my_playbook_ssh.command = secs[i].Key("command").String()
-                    ssh_playbooks = append(ssh_playbooks, my_playbook_ssh)
-                } else if ini_type == "scp" {
-                    var my_playbook_scp playbook_scp
-                    log.Println("dst is : %v", secs[i].Key("dst"))
-                    log.Println("src is : %v", secs[i].Key("src"))
-                    my_playbook_scp.dst = secs[i].Key("dst").String()
-                    my_playbook_scp.src = secs[i].Key("src").String()
-                    scp_playbooks = append(scp_playbooks, my_playbook_scp)
-                }
-        }
-        return scp_playbooks, ssh_playbooks, nil
+func parseini(path string) (playbooks []playbook, err error) {
+	cfg, err := ini.Load(path)
+	if err != nil {
+		log.Fatalf("open ini err: %v", err)
+		return nil, err
+		os.Exit(1)
+	}
+	var my_playbook playbook
+	var sshconfigs []sshconfig
+	secs := cfg.Sections()
+	for i := range secs {
+        ini_type := secs[i].Key("type").String()
+        my_playbook.name = secs[i].Name()
+		if ini_type == "hosts" {
+			ips := strings.Split(secs[i].Key("ips").String(), ",")
+			for j := range ips {
+				k, err := stringToSshconfig(ips[j])
+				if err != nil {
+					log.Fatalf("parseini error: %v", err)
+				}
+				sshconfigs = append(sshconfigs, k)
+			}
+			continue
+		}
+		// if operate type is ssh
+		if secs[i].HasKey("command") == true {
+			my_playbook.command = secs[i].Key("command").String()
+			my_playbook.playbook_type = "ssh"
+			my_playbook.dst = ""
+			my_playbook.src = ""
+			playbooks = append(playbooks, my_playbook)
+			continue
+			// if operate type is scp
+		} else if secs[i].HasKey("dst") == true && secs[i].HasKey("src") {
+			my_playbook.dst = secs[i].Key("dst").String()
+			my_playbook.src = secs[i].Key("src").String()
+			my_playbook.playbook_type = "scp"
+			my_playbook.command = ""
+			playbooks = append(playbooks, my_playbook)
+			continue
+		}
+	}
+	for i := range playbooks {
+		playbooks[i].servers = sshconfigs
+	}
+	return playbooks, nil
 }
 
-type playbook_ssh struct {
-    command string,
+func stringToSshconfig(line string) (myconfig sshconfig, err error) {
+	if strings.Contains(string(line), "@") && strings.Contains(string(line), ":") {
+		s := strings.Split(string(line), "@")
+		myconfig.user = s[0]
+		s1 := strings.Split(s[1], ":")
+		myconfig.address = s1[0]
+		myconfig.port = s1[1]
+	} else if strings.Contains(string(line), ":") == false && strings.Contains(string(line), "@") {
+		s := strings.Split(string(line), "@")
+		myconfig.user = s[0]
+		myconfig.address = s[1]
+		myconfig.port = "22"
+	} else if strings.Contains(string(line), "@") == false && strings.Contains(string(line), ":") {
+		myconfig.user = "root"
+		s := strings.Split(string(line), ":")
+		myconfig.address = s[0]
+		myconfig.port = s[1]
+	} else {
+		myconfig.user = "root"
+		myconfig.address = strings.Replace(string(line), "\n", "", -1)
+		if myconfig.address == "" {
+			log.Fatalf("stringToSshconfig error: line is blank!")
+			return myconfig, fmt.Errorf("stringToSshconfig error: line is blank!")
+		}
+		myconfig.port = "22"
+	}
+	return myconfig, nil
+
+}
+func parseHostfile(hostfile string) (result_sshconfig []sshconfig, err error) {
+	fi, err := os.Open(hostfile)
+	if err != nil {
+		fmt.Printf("parseHostfile.Open Error: %s\n", err)
+		return nil, err
+	}
+	br := bufio.NewReader(fi)
+	for {
+		line, err := br.ReadString('\n')
+		if err != nil || err == io.EOF {
+			break
+		}
+		myconfig, err := stringToSshconfig(line)
+		if err != nil {
+			log.Fatalf("parseHostfile.stringToSshconfig error: %v", err)
+			return nil, err
+		}
+		result_sshconfig = append(result_sshconfig, myconfig)
+	}
+	return result_sshconfig, nil
 }
 
-type playbook_scp struct {
-    dst string,
-    src string,
+type sshconfig struct {
+	user    string
+	address string
+	port    string
+}
+
+type playbook struct {
+    name          string
+	playbook_type string
+	src           string
+	dst           string
+	command       string
+	servers       []sshconfig
 }
