c524ac49488feea1153e44ebf3c54ceb4ef7ab54
git <dengyi0215@gmail.com>
2018-11-23 13:50:54 +0800

    unit test

---
+++ parseini_test.go
@@ -0,0 +1,20 @@
+package gotest
+
+import (
+	"testing"
+)
+
+func Test_Parseini(t *testing.T) {
+	playbooks, err := parseini("playbook.ini")
+	if err != nil {
+		t.Error("parseini error: %v", err)
+	}
+	my_playbook_1 := playbooks[0]
+	my_playbook_2 := playbooks[1]
+	if my_playbook_1.type == "scp" and my_playbook_1.type == "ssh" {
+		t.Log("parseini get type pass")
+	}
+	if my_playbook_1.src == "playbook.ini" && my_playbook_1.dst == "/home/playbook.ini" && my_playbook_2.command === "cat /home/playbook.ini" {
+		t.Log("get other paraments pass")
+	}
+}
