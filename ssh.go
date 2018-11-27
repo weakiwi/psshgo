84ccdac45286b299215bccc569734a8de1c3fabf
git <dengyi0215@gmail.com>
2018-11-27 21:37:25 +0800

    m

--- ssh.go
+++ ssh.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"io/ioutil"
+	"fmt"
 	"log"
 	"github.com/weakiwi/gosshtool"
 )
@@ -24,4 +25,40 @@ func make_a_connection(sc *sshconfig) (sshclient *gosshtool.SSHClient, err error
 	}
 	sshclient = gosshtool.NewSSHClient(config2)
 	return sshclient, nil
+}
+
+func sshexec_without_connect(sshclient *gosshtool.SSHClient, command string, done chan string) {
+	stdout, stderr, _, err := sshclient.Cmd(command, nil, nil, 0)
+	if err != nil {
+		waitgroup.Done()
+		log.Println("sshexec error is : ", err)
+		done <- fmt.Sprintf(stderr)
+		return
+	}
+	waitgroup.Done()
+	done <- fmt.Sprintf("%s[%s]%s\n%s", CLR_R, sshclient.SSHClientConfig.Host, CLR_N, stdout)
+	return
+}
+
+func scpexec_without_connection(client *gosshtool.SSHClient, srcfile string, destfile string, done chan string) {
+	f, err := os.Open(srcfile)
+	if err != nil {
+		return
+	}
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return
+	}
+	stdout, stderr, err := client.TransferData(destfile, data)
+	if err != nil {
+		log.Printf(stderr)
+	}
+	stdout, stderr, _, err = client.Cmd("md5sum "+destfile, nil, nil, 0)
+	if err != nil {
+		waitgroup.Done()
+		done <- fmt.Sprintf(stderr)
+	}
+	waitgroup.Done()
+	done <- fmt.Sprintf("%s[%s]%s\n%s", CLR_R, client.SSHClientConfig.Host, CLR_N, stdout)
+	return
 }
